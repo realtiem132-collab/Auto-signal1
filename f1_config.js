50c74a589cb2387e1dc7eca3542ae385de666384
realtiem132-collab <realtiem132@gmail.com>
2026-06-08 00:46:02 +0600

    Add files via upload

---
+++ f1_config.js
@@ -0,0 +1,29 @@
+// f1_config.js
+
+module.exports = {
+
+    // আপনার টেলিগ্রাম বট টোকেন (BotFather থেকে প্রাপ্ত)
+
+    TELEGRAM_TOKEN: '8806117889:AAHfxPbHkDmImG7DFgWy0qXWXNS62U9-qiw', 
+
+    
+
+    // Quotex Real Market Pairs
+
+    PAIRS: [
+
+        'EURUSD', 'GBPUSD', 'USDJPY', 'AUDUSD', 
+
+        'USDCAD', 'EURGBP', 'EURJPY', 'GBPJPY'
+
+    ],
+
+    
+
+    // টাইমজোন সেটিংস
+
+    TIMEZONE: 'Asia/Dhaka', // বাংলাদেশ টাইম শো করার জন্য
+
+    INTERVAL: '1m' // 1 minute candle
+
+};
